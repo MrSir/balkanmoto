205eba43e6048011116bd041a26aad44bd640cd0
Mitko Tochev <mr.sir88@gmail.com>
2018-08-08 15:48:36 -0400

    implemented more of the small mobile view layout

--- resources/views/theme/footer.blade.php
+++ resources/views/theme/footer.blade.php
@@ -1,17 +1,36 @@
 <div class="footer-grid grid-item">
-    <div class="tweets grid-item">
+    <div class="tweeter grid-item">
         <div class="footer-title">Tweets</div>
+        <div class="tweets">
+            <div class="tweet">
+                <img src="" width="50px" height="50px">
+                <div class="name">BalkanMoto</div>
+                <div class="handle"><a href="#">@BalkanMoto2018</a></div>
+                <div class="message">Check out the latest Honda Phantom Build <a href="#">bit.log/1235had</a></div>
+            </div>
+        </div>
     </div>
     <div class="links grid-item">
         <div class="footer-title">Links</div>
         <ul>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Contact</li>
-            <li>Log In</li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">ARTICLES</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a href="#">LOG IN</a></li>
         </ul>
     </div>
     <div class="social grid-item">
         <div class="footer-title">Social</div>
+        <div class="icons">
+            <div class="youtube">
+                <a href="#"><span class="fab fa-youtube"></span></a>
+            </div>
+            <div class="instagram">
+                <a href="#"><span class="fab fa-instagram"></span></a>
+            </div>
+            <div class="twitter">
+                <a href="#"><span class="fab fa-twitter-square"></span></a>
+            </div>
+        </div>
     </div>
 </div>
--- resources/views/theme/master.blade.php
+++ resources/views/theme/master.blade.php
@@ -8,6 +8,8 @@
     <title>Balkan Moto</title>
 
     <!-- Fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+          integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
 
     <link href="css/style.css" rel="stylesheet">
