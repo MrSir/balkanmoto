9608ee343efd31195c1a9559c610a504d95ceb4b
Mitko Tochev <mr.sir88@gmail.com>
2018-08-19 13:08:46 -0400

    added mobile friendly support

--- resources/views/pages/articles/create.blade.php
+++ resources/views/pages/articles/create.blade.php
@@ -31,7 +31,11 @@
           toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
           content_css: [
             '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-            '//www.tinymce.com/css/codepen.min.css']
+            '//www.tinymce.com/css/codepen.min.css'
+          ],
+          mobile: {
+            theme: 'mobile',
+          }
         }
       )
     </script>
