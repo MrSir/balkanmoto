679ef0d306e9d886fdb9ebe9dc6314d888ccd2e9
Mitko Tochev <mr.sir88@gmail.com>
2018-08-21 16:38:51 -0400

    quick styling for the edit form

--- resources/views/pages/articles/edit.blade.php
+++ resources/views/pages/articles/edit.blade.php
@@ -1,25 +1,23 @@
 @extends('theme.master')
 
 @section('page-header')
-    {{--<link href="/css/pages/article-style.css" rel="stylesheet">--}}
-    {{--<link href="/css/pages/article-large-style.css" rel="stylesheet">--}}
+    <link href="/css/pages/article-style.css" rel="stylesheet">
+    <link href="/css/pages/article-large-style.css" rel="stylesheet">
 
     <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=zys0yf760xr9gjmcreu1cvsoko9mje67of20stvs3tk7bhau"></script>
 @endsection
 
 @section('content')
-    <div class="post-grid">
-        <form id="edit" name="edit" method="POST" action="/articles/1">
-            {{ csrf_field() }}
-            <input type="hidden" id="_method" name="_method" value="PUT"/>
-            <input type="checkbox" id="published" name="published"/>
-            <input type="file" id="image" name="image"/>
-            <input type="text" id="title" name="title"/>
-            <textarea id="body" name="body"></textarea>
-            <input type="text-area" id="tags" name="tags"/>
-            <input type="submit" id="submit" name="submit" value="SAVE"/>
-        </form>
-    </div>
+    <form id="edit" name="edit" method="POST" action="/articles/1">
+        {{ csrf_field() }}
+        <input type="hidden" id="_method" name="_method" value="PUT"/>
+        <input type="checkbox" id="published" name="published"/>
+        <input type="file" id="image" name="image"/>
+        <input type="text" id="title" name="title"/>
+        <textarea id="body" name="body"></textarea>
+        <input type="text-area" id="tags" name="tags"/>
+        <input type="submit" id="submit" name="submit" value="SAVE"/>
+    </form>
     <script type="text/javascript">
       tinymce.init(
         {
