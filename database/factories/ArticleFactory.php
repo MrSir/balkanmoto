9ddec2a2c236e59bb664d0b71aebd7297e4696d5
Mitko Tochev <mr.sir88@gmail.com>
2018-08-28 16:37:44 -0400

    fixed the factory

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -13,7 +13,7 @@ $factory->define(\App\Article::class, function (Faker $faker) {
         'slug' => $faker->word,
         'is_published' => $faker->boolean,
         'is_featured' => $faker->boolean,
-        'user_id' => \App\Image::first(),
+        'user_id' => \App\User::first(),
         'image_id' => \App\Image::first(),
         'title' => $faker->sentence,
         'summary' => $faker->sentence,
