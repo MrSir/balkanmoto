189b6018046b4aedc0f1b2e2cc4c5726b787c059
Mitko Tochev <mr.sir88@gmail.com>
2018-11-05 06:39:03 -0500

    added image caching

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Image;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 
 /**
@@ -13,24 +14,24 @@ use Illuminate\Support\Facades\Storage;
 class ImageController extends Controller
 {
     /**
-     * Display the specified resource.
-     *
      * @param Request $request
      * @param $id
      *
-     * @return string
+     * @return Response
      * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     public function show(Request $request, $id)
     {
         $image = Image::find($id);
-
         $input = $request->input();
 
+        $response = (new Response())
+            ->setExpires(now()->addMonth());
+
         if (array_key_exists('thumb', $input)) {
-            return Storage::get($image->thumbnail);
+            return $response->setContent(Storage::get($image->thumbnail));
         }
 
-        return Storage::get($image->filename);
+        return $response->setContent(Storage::get($image->filename));
     }
 }
