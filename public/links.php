470f69899f703ccf85e78fcac20bd6945c5607eb
Mitko Tochev <mr.sir88@gmail.com>
2024-12-22 13:11:16 -0500

    implemented links and deleted old files

---
+++ public/links.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en-ca">
+<head>
+    <?php include("./theme/header-meta.php"); ?>
+
+    <link href="/css/pages/links-style.css" rel="stylesheet">
+    <link href="/css/pages/links-large-style.css" rel="stylesheet">
+</head>
+<body>
+<div class="content container-grid">
+    <?php include("./theme/menu.php"); ?>
+    <?php include("./theme/header.php"); ?>
+    <div class="divider">LINKS</div>
+    <div class="content-grid">
+        <div class="content-center">
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEVbNL24JMC3hxkyewp6R0D-" target="_blank">Series: The Wheelbarrow CX</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEU-3mP-xKq_cn66aQr-gd4i" target="_blank">Series: The Jocker Bobber</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEUa_8GHV-5bgc0IVmPIID7S" target="_blank">Series: How a V45 Engine Works</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEU7KSyNKcsSCwaU-hIOQi77" target="_blank">Series: How to fix a Cafe Racer</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEWRmc_wHvq9fMHSfcXa5QiO" target="_blank">Series: How to build a Cafe Racer</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEUoCdY5e8cBkvoeEB__R3_s" target="_blank">Series: Tutorials</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEXe8HSv3ejL-yqJRIVJ8rHQ" target="_blank">Series: The Cafe Race - Season 2 (2021/2022)</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEXe8HSv3ejL-yqJRIVJ8rHQ" target="_blank">Series: The Cafe Race - Season 1 (2020)</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEVsIgIc6UkMALWlZbIzI8ZF" target="_blank">Series: How to build a BOSS Bobber</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEUwQQ0EVe-7pbXlaqWjF-lI" target="_blank">Assetto Corsa Mods</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/playlist?list=PLPOp2IhKEuEUwQQ0EVe-7pbXlaqWjF-lI" target="_blank">Assetto Corsa Mods</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.youtube.com/channel/UCHbSArk-BwFhFQmtV_ws2Ng" target="_blank">YouTube</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://www.instagram.com/balkanmoto2018/" target="_blank">Instagram</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="https://ko-fi.com/balkanmoto" target="_blank">Ko-fi</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include("./theme/footer.php"); ?>
+</div>
+</body>
+</html>
--- resources/views/pages/privacy.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('theme.master')
-
-@section('page-header')
-
-@endsection
-
-@section('content')
-    Coming Soon ...
-@endsection
