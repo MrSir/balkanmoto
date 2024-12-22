1ff03cbd0e62a2b211290b10f21f01fbe09509d6
Mitko Tochev <mr.sir88@gmail.com>
2024-12-22 13:59:52 -0500

    simplified styling

--- public/builds.php
+++ public/builds.php
@@ -3,8 +3,8 @@
 <head>
     <?php include("./theme/header-meta.php"); ?>
 
-    <link href="/css/pages/article-style.css" rel="stylesheet">
-    <link href="/css/pages/article-large-style.css" rel="stylesheet">
+    <link href="/css/pages/links-style.css" rel="stylesheet">
+    <link href="/css/pages/links-large-style.css" rel="stylesheet">
 </head>
 <body>
 <div class="content container-grid">
@@ -12,17 +12,26 @@
     <?php include("./theme/header.php"); ?>
     <div class="divider">BUILDS</div>
     <div class="content-grid">
-        <div class="post-grid">
-            <div class="body">
-                <p>Here you will find a list of all of the builds I have done over the years.</p>
-                <p>Builds:
-                    <ul>
-                        <li><a href="/builds/wheelbarrow-cx.php">The Wheelbarrow CX - Wooden Push Bike</a></li>
-                        <li><a href="/builds/2002-yamaha-vstar-1100-bobber.php">2002 Yamaha V-Star 1100 Bobber</a></li>
-                        <li><a href="/builds/2013-suzuki-c90t-boss-bobber.php">2013 Suzuki C90T BOSS Bobber</a></li>
-                        <li><a href="/builds/1983-honda-sabre-v45-cafe-racer.php">1983 Honda Sabre V45 Cafe Racer</a></li>
-                    </ul>
-                </p>
+        <div class="content-center">
+            <div class="link">
+                <div class="box">
+                    <a href="/builds/wheelbarrow-cx.php">The Wheelbarrow CX - Wooden Push Bike</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="/builds/2002-yamaha-vstar-1100-bobber.php">2002 Yamaha V-Star 1100 Bobber</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="/builds/2013-suzuki-c90t-boss-bobber.php">2013 Suzuki C90T BOSS Bobber</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="/builds/1983-honda-sabre-v45-cafe-racer.php">1983 Honda Sabre V45 Cafe Racer</a>
+                </div>
             </div>
         </div>
     </div>
--- public/tools.php
+++ public/tools.php
@@ -3,8 +3,8 @@
 <head>
     <?php include("./theme/header-meta.php"); ?>
 
-    <link href="/css/pages/article-style.css" rel="stylesheet">
-    <link href="/css/pages/article-large-style.css" rel="stylesheet">
+    <link href="/css/pages/links-style.css" rel="stylesheet">
+    <link href="/css/pages/links-large-style.css" rel="stylesheet">
 </head>
 <body>
 <div class="content container-grid">
@@ -12,15 +12,16 @@
     <?php include("./theme/header.php"); ?>
     <div class="divider">TOOLS</div>
     <div class="content-grid">
-        <div class="post-grid">
-            <div class="body">
-                <p>Here you will find some web based tools I've built that can help you with understanding on configuring real world things</p>
-                <p>Tools:
-                    <ul>
-                        <li><a href="/tools/suspension-geometry.php">SUSPENSION GEOMETRY</a></li>
-                        <li><a href="/tools/carburetor-tuning.php">CARBURETOR TUNING</a></li>
-                    </ul>
-                </p>
+        <div class="content-center">
+            <div class="link">
+                <div class="box">
+                    <a href="/tools/suspension-geometry.php">SUSPENSION GEOMETRY</a>
+                </div>
+            </div>
+            <div class="link">
+                <div class="box">
+                    <a href="/tools/carburetor-tuning.php">CARBURETOR TUNING</a>
+                </div>
             </div>
         </div>
     </div>
