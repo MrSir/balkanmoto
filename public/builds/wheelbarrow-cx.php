dd7a1871b7f3f9f2f46050aebf3db78f13a66924
Mitko Tochev <mr.sir88@gmail.com>
2024-12-27 14:45:36 -0500

    wheelbarrow cx page

--- public/builds/wheelbarrow-cx.php
+++ public/builds/wheelbarrow-cx.php
@@ -14,7 +14,28 @@
     <div class="content-grid">
         <div class="post-grid">
             <div class="body">
-                TODO
+                <h2>The Idea</h2>
+                <p>In 2023 I become a father to a lovely little boy. So immediately I thought how can I start getting him involved with motorcycles at this age. Actual motorcycles were out of the question as they were too big and heavy. Bicycles required balance that he didn't yet have. Push bikes were however something that could most definitely be an option. So I started to do research on what options for push bikes exists out there. Some years ago there was a company that used to make wooden push bikes that looked like cafe racers, but it seemed to had closed it's business. So I though why not design and build one myself.</p>
+                <p>First thing that popped into my mind when cafe racer came to mind was the CX500 builds by BlackTrack (dubbed BT-01). I pulled up my 3D software and started to model something out of simple shapes that reselbed one. Soon enough I had a full design for a push bike that was to be built out of easily accessible materials from the hardware store.</p>
+                <img src="/builds/img/wheelbarrow-cx/design.png">
+
+                <h2>The Build</h2>
+                <p>Armed with a cordless drill, a mitar saw, and sander I started the long process of cutting and shaping all of the 2"x4" pieces to size. Most of the pieces were simple cuts. The forks were the only thing that required more involved cutting, that took a few days to do. Could it have been done far more precisely and faster had a I acquired more specialized woodworking tools? Absolutely. But I wasn't about to spend the money for something that was very much a one off build.</p>
+                <p>From the hardware store I ended up picking up some wheels meant for heavy duty wheelbarrows to use for the bike. Most other wheel options for push bikes were either hard plastic which seemed like they would slip too easily, or didn't quite have the look I was going for. This is when the name for the project also came to be. I was mimicking a Honda CX 500, but with the wheels off a wheelbarrow...hence... Wheelbarrow CX.</p>
+
+                <h2>The Paint Job</h2>
+                <p>Using a few cans of spray paint I was able to bring to life my original 3D model.</p>
+                <img src="/builds/img/wheelbarrow-cx/painted.jpg">
+                <img src="/builds/img/wheelbarrow-cx/painted_2.jpg">
+                <img src="/builds/img/wheelbarrow-cx/painted_3.jpg">
+
+                <h2>Conclusion</h2>
+                <p>With the build more or less complete, still needs a seat, I did a little test ride with my son...and came to realize something. The push bike was still far too big for him, and weight nearly as much as he does. So the bike went on a shelf as an art piece in my garage, until my son is big enough to actually be able to push it around.</p>
+                <img src="/builds/img/wheelbarrow-cx/waiting.jpg">
+
+                <h2>The Wheelbarrow CX Series</h2>
+                <p>This is the series that chronicles the build.</p>
+                <iframe width="1000" height="563" src="https://www.youtube.com/embed/videoseries?list=PLPOp2IhKEuEVbNL24JMC3hxkyewp6R0D-" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     </div>
