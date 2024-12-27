09a6ba69cde1a84d9e2e6fb148104d8f497f7b66
Mitko Tochev <mr.sir88@gmail.com>
2024-12-27 15:38:27 -0500

    placeholder for leathercraft pages

--- public/leather-craft.php
+++ public/leather-craft.php
@@ -3,8 +3,8 @@
 <head>
     <?php include("./theme/header-meta.php"); ?>
 
-    <link href="/css/pages/links-style.css" rel="stylesheet">
-    <link href="/css/pages/links-large-style.css" rel="stylesheet">
+    <link href="/css/pages/articles-style.css" rel="stylesheet">
+    <link href="/css/pages/articles-large-style.css" rel="stylesheet">
 </head>
 <body>
 <div class="content container-grid">
@@ -12,12 +12,19 @@
     <?php include("./theme/header.php"); ?>
     <div class="divider">LEATHER CRAFT</div>
     <div class="content-grid">
-        <div class="content-center">
-            <div class="link">
-                <div class="box">
-                    <a href="">TBD</a>
-                </div>
-            </div>
+        <div class="grid-item main-grid">
+              <div class="post-grid">
+                  <img src="/leather-craft/img/suspension_geometry.jpg" alt="Article Cover">
+                  <div class="title"><a href="/leather-craft/the-jocker-bobber-seat.php">The Jocker Bobber Seat</a></div>
+              </div>
+              <div class="post-grid">
+                  <img src="/leather-craft/img/carb_tunning.jpg" alt="Article Cover">
+                  <div class="title"><a href="/leather-craft/the-cafe-sabre-saddlebags.php">The Cafe Sabre Saddlebags</a></div>
+              </div>
+              <div class="post-grid">
+                  <img src="/leather-craft/img/carb_tunning.jpg" alt="Article Cover">
+                  <div class="title"><a href="/leather-craft/the-balkanmoto-bbq-apron.php">The BalkanMoto BBQ Apron</a></div>
+              </div>
         </div>
     </div>
     <?php include("./theme/footer.php"); ?>
---
+++ public/leather-craft/the-jocker-bobber-seat.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en-ca">
+<head>
+    <?php include("../theme/header-meta.php"); ?>
+
+    <link href="/css/pages/article-style.css" rel="stylesheet">
+    <link href="/css/pages/article-large-style.css" rel="stylesheet">
+</head>
+<body>
+<div class="content container-grid">
+    <?php include("../theme/menu.php"); ?>
+    <?php include("../theme/header.php"); ?>
+    <div class="divider">THE JOCKER BOBBER SEAT</div>
+    <div class="content-grid">
+        <div class="post-grid">
+            <div class="body">
+                TBD
+            </div>
+        </div>
+    </div>
+    <?php include("../theme/footer.php"); ?>
+</div>
+</body>
+</html>
