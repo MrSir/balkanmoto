8e08762d714a464f71f89e61f862cd4e814f1dc6
Mitko Tochev <mr.sir88@gmail.com>
2024-12-27 11:41:44 -0500

    added suzuki build

--- public/builds/2013-suzuki-c90t-boss-bobber.php
+++ public/builds/2013-suzuki-c90t-boss-bobber.php
@@ -14,7 +14,42 @@
     <div class="content-grid">
         <div class="post-grid">
             <div class="body">
-                TODO
+                <h2>The Backstory</h2>
+                <p>While building my <a href="/builds/1982-83-honda-sabre-v45-cafe-racer.php">Cafe Sabre</a> I had started to outgrow my everyday bike. A 2011 Honda Shadow Phantom. I was on the look out for a new bigger cruiser that would match my riding style, and lend itself to some minor modifications. It wasn't long before I stumbled on a 2013 Suzuki C90T BOSS that was a steal for the price it was listed for.</p>
+                <img src="/builds/img/2013-suzuki-c90t-boss-bobber/stock.jpg">
+                <img src="/builds/img/2013-suzuki-c90t-boss-bobber/stock_2.jpg">
+
+                <h2>The Strip Down</h2>
+                <p>The bike came with a full touring package, however at the time I rode mostly in the city and the extra wait and wind protection was simply not needed. So it had to go. As soon as I had brought the bike home, I ended up stripping it down from it's extra parts. I removed the following components:
+                    <ul>
+                        <li>The windshield, along with all of the supporting brackets.</li>
+                        <li>The saddle bags, along with all of the supporting brackets.</li>
+                        <li>The seat rack.</li>
+                        <li>The rider and pillion seat back rests.</li>
+                    </ul>
+                </p>
+
+                <h2>The Modifications</h2>
+                <p>I had been lured in to the bike by it's blacked out looked, and hadn't done enough research on it to find out that there wasn't actually much of an aftermarket for these bikes. Or at least not for these newer versions of them. The older VL1500 models had quite a bit of a following, but these new ones not so much. This pretty much meant that any mods I did would end up having to be quite custom. So I ended up focusing mostly on aesthetics and a little bit of functionality. Over the coming years of my ownership of the bike I ended up doing the following mods:
+                    <ul>
+                        <li>Installed 14" Ape Hangers.</li>
+                        <li>Replaced the front break lines.</li>
+                        <li>Replaced the throttle cables.</li>
+                        <li>Replaced the clutch cable.</li>
+                        <li>Extended the handle bar controls cables and internally wired them.</li>
+                        <li>Replaced the bar end weights with much heavier ones.</li>
+                        <li>Replaced mirrors with more slick ones.</li>
+                    </ul>
+                </p>
+
+                <h2>Conclusion</h2>
+                <p>After a couple of years of riding the Suzuki, I came to realize that displacement and power are not everything. I found that in most of my riding, even on quite country roads, I was not utilizing the motorcycle to it's full potential. That potential was only realized on long highway halls, which I never did. That combined with a fairly limited aftermarket ultimately lead me to sell the bike off. So moral of the story is do your research and try things.</p>
+                <img src="/builds/img/2013-suzuki-c90t-boss-bobber/modified.jpg">
+                <img src="/builds/img/2013-suzuki-c90t-boss-bobber/modified_2.jpg">
+
+                <h2>How to build a BOSS Bobber Series</h2>
+                <p>This is a short series chronicling the build.</p>
+                <iframe width="1000" height="563" src="https://www.youtube.com/embed/videoseries?list=PLPOp2IhKEuEVsIgIc6UkMALWlZbIzI8ZF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     </div>
