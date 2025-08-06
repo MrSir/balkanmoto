d3f63ea0ae77852c5c50a09754e31c8eb900aaf7
Mitko Tochev <mr.sir88@gmail.com>
2025-08-05 21:34:46 -0400

    added skeleton and gpx files

---
+++ public/adventures/acton-trails.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en-ca">
+<head>
+    <?php include("../theme/header-meta.php"); ?>
+
+    <link href="/css/pages/article-style.css" rel="stylesheet">
+    <link href="/css/pages/article-large-style.css" rel="stylesheet">
+</head>
+<body>
+<div class="content container-grid">
+    <?php include("../theme/menu.php"); ?>
+    <?php include("../theme/header.php"); ?>
+    <div class="divider">ACTON TRAILS</div>
+    <div class="content-grid">
+        <div class="post-grid">
+            <div class="body">
+                <h2>The Map</h2>
+                <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1_lWqLMwL_uNr3InlNgoPCuz9zd5Lh1U&ehbc=2E312F" height="500"></iframe>
+
+                <h2>Main Path</h2>
+                <p>TBD</p>
+
+                <h2>Loop 1: Beginner Loop</h2>
+                <p>TBD</p>
+
+                <h2>Loop 2: Beginner/Intermediate Loop</h2>
+                <p>TBD</p>
+
+                <h2>GPX Files</h2>
+                <ul>
+                    <li><a href="/adventures/gpx/acton_trails_main_path.gpx" target="_blank">Main Path</a></li>
+                    <li><a href="/adventures/gpx/acton_trails_loop_1.gpx">Loop 1</a></li>
+                    <li><a href="/adventures/gpx/acton_trails_loop_2.gpx">Loop 2</a></li>
+                </ul>
+                <h2>Videos</h2>
+                <iframe src="https://www.youtube.com/embed/mh7_5geqrVg?si=tOHv22Cc7XdVl1-a" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+    <?php include("../theme/footer.php"); ?>
+</div>
+</body>
+</html>
