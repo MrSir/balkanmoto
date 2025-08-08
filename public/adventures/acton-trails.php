d43a10605cd01b62909db2aa3862d81a31820e40
Mitko Tochev <mr.sir88@gmail.com>
2025-08-07 21:07:31 -0400

    final version of page

--- public/adventures/acton-trails.php
+++ public/adventures/acton-trails.php
@@ -18,13 +18,13 @@
                 <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1_lWqLMwL_uNr3InlNgoPCuz9zd5Lh1U&ehbc=2E312F" height="500"></iframe>
 
                 <h2>Main Path</h2>
-                <p>TBD</p>
+                <p>The main trail through the Acton farm land is actually more or less just a dirt road for farm equipment. It is used by the locals as a walking path, and by the off road enthusiasts as a riding ground. The north entrance is just at the east edge of town off of HWY 7. The south entrance is at the south edge of town just off of Churchill Road. The trail is very beginner friendly, with lots of different terrain elements. The one thing that is predominant however is that on drier days the bulk of the road is a soft almost dust like sand that is about 1"-2" deep. This could easily sway you one way or another. Just off of the main path towards loop 1, there is a slightly bigger hill with a steeper incline. It's a great place to practice going up and down. The south side of the hill is a bit more challenging with a very long straight descend at about 20deg incline. What makes it more challenging is the large number of loose medium size rocks, that make choosing your line a bit more interesting.</p>
 
                 <h2>Loop 1: Beginner Loop</h2>
-                <p>TBD</p>
+                <p>The first loop starts on the flat training ground area and follows the single track towards the forest. The loop itself is fairly short, but gets you exposed to some single track, some fairly easy hills, and some sand and medium size rock filled areas. All in all quite easy, but a solid spot to practice fundamentals and work on slow speed maneuvers.</p>
 
                 <h2>Loop 2: Beginner/Intermediate Loop</h2>
-                <p>TBD</p>
+                <p>The second loop shares a part of the main path with loop 1. It veers off through a sandy forest path towards one of the farm fields to the east. Going through the field is a straight path following the perimeter, that is fairly grassy, so traction could be a fun surprise to the throttle happy. The north end of the loop goes through another tight forest section that leads back to the main path.</p>
 
                 <h2>GPX Files</h2>
                 <ul>
@@ -33,7 +33,7 @@
                     <li><a href="/adventures/gpx/acton_trails_loop_2.gpx">Loop 2</a></li>
                 </ul>
                 <h2>Videos</h2>
-                <iframe src="https://www.youtube.com/embed/mh7_5geqrVg?si=tOHv22Cc7XdVl1-a" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/17_xt-jsxJs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     </div>
