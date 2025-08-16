11f2bf2f5b85f5d42438307223216eb9d2e62c98
Mitko Tochev <mr.sir88@gmail.com>
2025-08-15 21:28:06 -0400

    campbelleville quarry

--- public/adventures/campbellville-quarry.php
+++ public/adventures/campbellville-quarry.php
@@ -18,21 +18,21 @@
                 <iframe src="https://www.google.com/maps/d/u/2/embed?mid=1Mcye1oZZ5l2wEkOIRcb1IIoUFkfnXgo&ehbc=2E312F" height="500"></iframe>
 
                 <h2>The Adventure</h2>
-                <p></p>
+                <p>In this adventure I decided to explore the hidden trails of the old Campbellville Quarry. I had heard that the area presented some fairly easy forest tracks to ride and figured let's take my 210kg adventure big on them. To my surprise the area was actually super nice to ride. The trails varied in difficulty but mostly stay fairly beginner. Most are plenty wide to turn around at any point, not that you will need to.</p>
 
                 <h2>Loop 1: Beginner Loop</h2>
-                <p></p>
+                <p>The first trail I explored very pleasantly surprised me. Most of seemed to have been defined by an ATV. So it was nice and wide, and easy to navigate. From the main path the trail takes you immediately into a table top style hill. I am sure more experienced rider could probably catch some series air off this thing. Then the track proceeds through a well packed dirt path looping around back to the main path. The second section of the loop has a nice light downhill section followed by a couple of meters of deep ruts. If you go there when it's wet you will likely have a bit of fun in the ruts, like it or not.</p>
 
                 <h2>Loop 2: Beginner/Intermediate Loop</h2>
-                <p></p>
+                <p>The second loop at first didn't look like a loop at all. It just looked like a path that leads to a dead end in the forest. Turns out a cleverly done loop at the end takes you back onto the path you came on leading you back out. The beginning of it is technically the same as the first loop. Going over the table top section, and then turning into the forest. The first significant obstacle here is a fairly large sand pit. If you keep your momentum up it should be no problem getting through it. After that you enter into a hard packed forest section with a number of large rocks embedded into the soil sticking out. Picking a good line makes this a breeze as well. Once you go around the loop, the very last bit of it, gets a little short with branches over head, making it nearly impossible to stand while riding. Then you are off the way you came going through the same obstacles.</p>
 
                 <h2>Lake Loop: Beginner Loop with a view</h2>
-                <p></p>
+                <p>The lake loop is probably my favorite one of all of them. It follows the perimeter of the first lake (flooded quarry) that you see when you enter the area. Initially it's slightly sandy and quite easy. Once you make it around the corner however you are quickly faced with a hill, that leads you into the pine forest. This part of the trail has a hard packed surface with 2 tracks almost step like next to each other. The fact that it's in a pine forest also means there are plenty of roots sticking out and pine needles covering the floor, making things slippery. Once you get past that section you end up on the center peninsula in the lake. This is a great spot to stop, take a photo, and maybe rest if you had been riding for a while. From there you go back into the pine forest and off towards the train tracks. Keep an eye for low hanging branches, it's quite easy to clip your head on things when standing. Once you reach the train tracks you hang a left and follow the gravel back to the main path. Heads up the gravel gets really deep right by the main path, go around it.</p>
 
                 <h2>GPX Files</h2>
                 <ul>
-                    <li><a href="/adventures/gpx/campbellville_quarry_start_2_lunch.gpx" target="_blank">Start to Lunch</a></li>
-                    <li><a href="/adventures/gpx/campbellville_quarry_lunch_2_start.gpx" target="_blank">Lunch to Start</a></li>
+                    <li><a href="/adventures/gpx/campbellville_quarry_road_loop.gpx" target="_blank">Road Loop</a></li>
+                    <li><a href="/adventures/gpx/campbellville_quarry_main_path.gpx" target="_blank">Main Path</a></li>
                     <li><a href="/adventures/gpx/campbellville_quarry_loop_1.gpx" target="_blank">Loop 1</a></li>
                     <li><a href="/adventures/gpx/campbellville_quarry_loop_2.gpx" target="_blank">Loop 2</a></li>
                     <li><a href="/adventures/gpx/campbellville_quarry_lake_loop.gpx" target="_blank">Lake Loop</a></li>
