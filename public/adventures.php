f119b67cd44b8ba77327771e537fd99ddf85b788
Mitko Tochev <mr.sir88@gmail.com>
2025-10-14 20:54:46 -0400

    adventures page done

--- public/adventures.php
+++ public/adventures.php
@@ -3,8 +3,8 @@
 <head>
     <?php include("./theme/header-meta.php"); ?>
 
-    <link href="/css/pages/articles-style.css" rel="stylesheet">
-    <link href="/css/pages/articles-large-style.css" rel="stylesheet">
+    <link href="/css/pages/adventures-style.css" rel="stylesheet">
+    <link href="/css/pages/adventures-large-style.css" rel="stylesheet">
 </head>
 <body>
 <div class="content container-grid">
@@ -12,27 +12,50 @@
     <?php include("./theme/header.php"); ?>
     <div class="divider">ADVENTURE ROUTES</div>
     <div class="content-grid">
-        <div class="grid-item main-grid">
-            <div class="post-grid">
-                <a href="/adventures/acton-trails.php"><img src="/adventures/img/acton-trails-logo.jpg" alt="Article Cover"></a>
-                <div class="summary">
-                    In this adventure I decided to explore the hidden trails of the old Campbellville Quarry. I had heard that the area presented some fairly easy forest tracks to ride and figured let's take my 210kg adventure big on them. To my surprise the area was actually super nice to ride. The trails varied in difficulty but mostly stay fairly beginner. Most are plenty wide to turn around at any point, not that you will need to.
-                </div>
-            </div>
-            <div class="post-grid">
-                <a href="/adventures/campbellville-quarry.php"><img src="/adventures/img/campbellville-quarry-logo.jpg" alt="Article Cover"></a>
-                <div class="summary">
-                    In this adventure I decided to explore the hidden trails of the old Campbellville Quarry. I had heard that the area presented some fairly easy forest tracks to ride and figured let's take my 210kg adventure big on them. To my surprise the area was actually super nice to ride. The trails varied in difficulty but mostly stay fairly beginner. Most are plenty wide to turn around at any point, not that you will need to.
-                </div>
-            </div>
-            <div class="post-grid">
-                <a href="/adventures/the-ridge.php"><img src="/adventures/img/the-ridge-logo.jpg" alt="Article Cover"></a>
-                <div class="summary">
-                    In this adventure I decided to explore the hidden trails of the old Campbellville Quarry. I had heard that the area presented some fairly easy forest tracks to ride and figured let's take my 210kg adventure big on them. To my surprise the area was actually super nice to ride. The trails varied in difficulty but mostly stay fairly beginner. Most are plenty wide to turn around at any point, not that you will need to.
-                </div>
-            </div>
-        </div>
+        <div id="main-grid" class="grid-item main-grid"></div>
     </div>
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function() {
+            const adventures = [
+                {
+                    "id": "acton-trails",
+                    "link": "/adventures/acton-trails.php",
+                    "logo": "/adventures/img/acton-trails-logo.jpg",
+                    "summary": "The perfect area for beginner off-road training. Lot's of easy terrain, with progressive difficulty. For hard packed dirt, to deep sand and steep rocky hills."
+                },
+                {
+                    "id": "campbellville-quarry",
+                    "link": "/adventures/campbellville-quarry.php",
+                    "logo": "/adventures/img/campbellville-quarry-logo.jpg",
+                    "summary": "A day trip in the Campbellville, Ontario area, with a small detour into the old quarry for some off-road trails."
+                },
+                {
+                    "id": "the-ridge",
+                    "link": "/adventures/the-ridge.php",
+                    "logo": "/adventures/img/the-ridge-logo.jpg",
+                    "summary": "A nice off-road trail that can be ridden from a fairly novice to a full enduro rider, with lots to offer in terms of exploration and varied terrain."
+                },
+            ]
+
+            const mainGrid = document.getElementById('main-grid');
+
+            adventures.forEach((adventure) => {
+                let postGridHTML = '<div id="' + adventure.id + '" class="post-grid"></div>'
+                mainGrid.innerHTML += postGridHTML
+
+                const postGrid = document.getElementById(adventure.id)
+                postGrid.innerHTML = '<img src="' + adventure.logo + '"/ alt="Adventure LOGO">'
+                postGrid.innerHTML += '<div class="summary">' + adventure.summary + '</div>'
+            })
+
+            adventures.forEach((adventure) => {
+                const postGrid = document.getElementById(adventure.id)
+                 postGrid.addEventListener("click", () => {
+                    window.location = adventure.link
+                 })
+            })
+        });
+    </script>
     <?php include("./theme/footer.php"); ?>
 </div>
 </body>
