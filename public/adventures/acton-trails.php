162a8b665a9decc66c537ca50b81c6d4c44a0834
Mitko Tochev <mr.sir88@gmail.com>
2025-10-16 20:30:44 -0400

    fix video link

--- public/adventures/acton-trails.php
+++ public/adventures/acton-trails.php
@@ -57,7 +57,7 @@
                 </ul>
                 <h2>Videos</h2>
                 <iframe src="https://www.youtube.com/embed/17_xt-jsxJs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                <iframe src="https://www.youtube.com/embed/17_xt-jsxJs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe src="https://www.youtube.com/embed/vKwfwLaNPdA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
         </div>
     </div>
