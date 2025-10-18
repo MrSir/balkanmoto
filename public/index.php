c476e2a949f2db4545f87b4556744909c6b47e1c
Mitko Tochev <mr.sir88@gmail.com>
2025-10-17 20:42:21 -0400

    home page styling

--- public/index.php
+++ public/index.php
@@ -30,6 +30,49 @@
                 <div class="title"><a href="/builds/1982-83-honda-sabre-v45-cafe-racer.php">1982-83 Honda Sabre V45 Cafe Racer - The Cafe Sabre</a></div>
             </div>
         </div>
+        <div id="adventures-grid"></div>
+        <script type="text/javascript">
+            document.addEventListener('DOMContentLoaded', function() {
+                const adventures = [
+                    {
+                        "id": "acton-trails",
+                        "link": "/adventures/acton-trails.php",
+                        "logo": "/adventures/img/acton-trails-logo.jpg",
+                        "summary": "The perfect area for beginner off-road training. Lot's of easy terrain, with progressive difficulty. From hard packed dirt, to deep sand and steep rocky hills."
+                    },
+                    {
+                        "id": "campbellville-quarry",
+                        "link": "/adventures/campbellville-quarry.php",
+                        "logo": "/adventures/img/campbellville-quarry-logo.jpg",
+                        "summary": "A day trip in the Campbellville, Ontario area, with a small detour into the old quarry for some off-road trails."
+                    },
+                    {
+                        "id": "the-ridge",
+                        "link": "/adventures/the-ridge.php",
+                        "logo": "/adventures/img/the-ridge-logo.jpg",
+                        "summary": "A nice off-road trail that can be ridden by any level rider, with lots to offer in terms of exploration and varied terrain."
+                    },
+                ]
+
+                const mainGrid = document.getElementById('adventures-grid');
+
+                adventures.forEach((adventure) => {
+                    let postGridHTML = '<div id="' + adventure.id + '" class="post-grid"></div>'
+                    mainGrid.innerHTML += postGridHTML
+
+                    const postGrid = document.getElementById(adventure.id)
+                    postGrid.innerHTML = '<img src="' + adventure.logo + '"/ alt="Adventure LOGO">'
+                    postGrid.innerHTML += '<div class="summary">' + adventure.summary + '</div>'
+                })
+
+                adventures.forEach((adventure) => {
+                    const postGrid = document.getElementById(adventure.id)
+                     postGrid.addEventListener("click", () => {
+                        window.location = adventure.link
+                     })
+                })
+            });
+        </script>
         <div class="other-grid">
             <div class="other">
                 <img src="/img/wall-art.jpg" alt="Article Cover">
